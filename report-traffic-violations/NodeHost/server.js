8befea0b61c7fa8b3b0d6b9d915b5e42a4600c60
Saurabh Sood <saurabhsood91@gmail.com>
2015-03-07 13:37:19 +0530

    refactor code and make db connections work

--- report-traffic-violations/NodeHost/server.js
+++ report-traffic-violations/NodeHost/server.js
@@ -1,12 +1,43 @@
- var express = require('express');
- image = require('./routes/image');
+var express = require('express');
+var mongo = require('mongoskin');
+var db = mongo.db('mongodb://localhost:27017/complaints', {
+	'database': 'test',
+	'safe': true
+});
+
+
+// var Server = mongo.Server,
+//       Db = mongo.Db;
+//       BSON = mongo.BSONPure;
+
+// var server = new Server('localhost',27017,{auto_reconnect: true});
+// db = new Db('winedb',server);
+
+// db.open(function(err,db)
+// {
+// 	if(!err)
+// 	{
+// 		console.log("Connected to traffic_violations database");
+// 		db.collection('complaint_list',{strict:true}, function(err,collection)
+// 		{
+// 			if(err)
+// 			{
+// 				console.log("The complaint_list collection doesnt exist.Creating it with sample data...");
+// 				//populateDB();
+// 			}
+// 		});
+// 	}
+
+// });
+
+ //image = require('./routes/image');
  complaints = require('./routes/complaints');
 
  var app = express();
 
- app.get('/image',image.findimage);
+ //app.get('/image',image.findimage(db));
 
- app.get('/complaints',complaints.getcomplaint);
+ app.get('/complaints',complaints.getcomplaint(db));
  
  app.listen(3000);
  
